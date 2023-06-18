2be70efb52acf73dde9d159b719a077279468086
Anton Kuznetsov <anton.kuznetcov@paybox.money>
2023-06-18 13:40:46 +0800

    db connection moved to Config. Added transaction_type model and handler

--- main.go
+++ main.go
@@ -1,65 +1,21 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-	_ "github.com/go-sql-driver/mysql"
 	"github.com/gofiber/fiber/v2"
-	"github.com/joho/godotenv"
 	"log"
-	"os"
-	"time"
+	"reward-coins-api/config"
 )
 
 func main() {
-	app := fiber.New()
-	db := getDb()
-
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Welcome to Go Fiber API")
-	})
-
-	startListening(app)
-
-	defer func(db *sql.DB) {
-		err := db.Close()
-		if err != nil {
-			log.Fatalf("Error closing the db connection")
-		}
-	}(db)
-}
-
-func getDataSource() string {
-	dbName := goDotEnvVariable("MYSQL_DATABASE")
-	user := goDotEnvVariable("MYSQL_USER")
-	password := goDotEnvVariable("MYSQL_PASSWORD")
-
-	return fmt.Sprintf("%s:%s@/%s", user, password, dbName)
-}
-
-func getDb() *sql.DB {
-	db, err := sql.Open("mysql", getDataSource())
-
-	// See "Important settings" section.
-	db.SetConnMaxLifetime(time.Minute * ConnectionsMaxLifetimeMin)
-	db.SetMaxOpenConns(MaxOpenConnections)
-	db.SetMaxIdleConns(MaxIdleConnections)
-
+	err := config.Connect()
 	if err != nil {
-		log.Fatalf(err.Error())
+		log.Printf("Error connecting to database")
 	}
+	app := *fiber.New()
+	SetupRoutes(&app)
 
-	return db
-}
-
-// use godot package to load/read the .env file and return the value of the key
-func goDotEnvVariable(key string) string {
-	// load .env file
-	err := godotenv.Load(".env")
-
+	err = app.Listen(ApplicationPort)
 	if err != nil {
-		log.Fatalf("Error loading .env file")
+		log.Fatalf("Error starting the app")
 	}
-
-	return os.Getenv(key)
 }
--- models/transaction_type.go
+++ models/transaction_type.go
@@ -1,5 +1,9 @@
 package models
 
+import "gorm.io/gorm"
+
 type TransactionType struct {
+	gorm.Model
+	ID   int    `json:"id"`
 	Name string `json:"name"`
 }
