fb645ed61a44d6a63977e647f92348c4cc742a23
antonkc <anton.kuznetcov@freedompay.money>
2023-11-05 22:23:57 +0600

    changed CORS configs

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 	app := fiber.New()
 	app.Use(cors.New(cors.Config{
 		AllowOrigins: "http://localhost:3000", // react app
-		AllowMethods: "GET,POST,PUT,DELETE",
+		AllowMethods: "GET,POST,PATCH,DELETE",
 	}))
 
 	SetupRoutes(app)
